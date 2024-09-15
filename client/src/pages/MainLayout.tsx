86b75ee64d85af2f0369090e130739366471d62e
Ali Banai <abanaei@dblexchange.com>
2024-09-15 04:24:08 +0330

    lint

--- client/src/pages/MainLayout.tsx
+++ client/src/pages/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useAppDispatch } from "../hooks/reduxHooks";
 import { getUser } from "../store/slices/userSlice";
 import { doLogout } from "../store/slices/authSlice";
